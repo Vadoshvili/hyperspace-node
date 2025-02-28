dd7589f03d24f106b90e42803165aa7ec5d809f5
Vadoshvili <167355070+Vadoshvili@users.noreply.github.com>
2025-02-28 21:42:21 +0300

    1Update hyperspace.sh

--- hyperspace.sh
+++ hyperspace.sh
@@ -1,12 +1,12 @@
- 
-  echo -e "\n\nПодпишись https://t.me/teapot_crypto
+channel_logo() { 
+  echo -e "\n\nПодпишись https://t.me/teapot_crypto"
 }
 
 download_node() {
   echo 'Начинаю установку...'
 
   read -p "Введите ваш приватный ключ: " PRIVATE_KEY
-  echo $PRIVATE_KEY > $HOME/my.pem
+  echo "$PRIVATE_KEY" > $HOME/my.pem
 
   session="hyperspacenode"
 
@@ -20,7 +20,7 @@ download_node() {
     aios-cli kill
   fi
   
-  if screen -list | grep -q "\.${session}"; then
+  if screen -list | grep -q "\.${session}\b"; then
     screen -S hyperspacenode -X quit
   else
     echo "Сессия ${session} не найдена."
@@ -47,9 +47,7 @@ download_node() {
 
   screen -dmS hyperspacenode bash -c '
     echo "Начало выполнения скрипта в screen-сессии"
-
     aios-cli start
-
     exec bash
   '
 
@@ -57,7 +55,7 @@ download_node() {
     aios-cli models add hf:TheBloke/phi-2-GGUF:phi-2.Q4_K_M.gguf 2>&1 | tee $HOME/hyperspacemodel_download.log
 
     if grep -q "Download complete" $HOME/hyperspacemodel_download.log; then
-        echo "Модель была установлен."
+        echo "Модель была установлена."
         break
     else
         echo "Сервер установки модели недоступен, повторим через 30 секунд..."
@@ -71,78 +69,3 @@ download_node() {
   aios-cli hive login
   aios-cli hive connect
 }
-
-check_logs() {
-  screen -S hyperspacenode -X hardcopy /tmp/screen_log.txt && sleep 0.1 && tail -n 100 /tmp/screen_log.txt && rm /tmp/screen_log.txt
-}
-
-check_points() {
-  aios-cli hive points
-}
-
-restart_node() {
-  session="hyperspacenode"
-  
-  if screen -list | grep -q "\.${session}"; then
-    screen -S "${session}" -p 0 -X stuff "^C"
-    sleep 1
-    screen -S "${session}" -p 0 -X stuff "aios-cli start --connect\n"
-    echo "Нода была перезагружена."
-  else
-    echo "Сессия ${session} не найдена."
-  fi
-}
-
-delete_node() {
-  read -p 'Если уверены удалить ноду, введите любую букву (CTRL+C чтобы выйти): ' checkjust
-
-  echo 'Начинаю удалять ноду...'
-
-  screen -S hyperspacenode -X quit
-  aios-cli kill
-  aios-cli models remove hf:TheBloke/phi-2-GGUF:phi-2.Q4_K_M.gguf
-  sudo rm -rf $HOME/.aios
-
-  echo 'Нода была удалена.'
-}
-
-exit_from_script() {
-  exit 0
-}
-
-while true; do
-    channel_logo
-    sleep 2
-    echo -e "\n\nМеню:"
-    echo "1. 🙂 Установить ноду"
-    echo "2. 📜 Посмотреть логи"
-    echo "3. ⭐ Узнать сколько поинтов"
-    echo "4. 🔄 Перезагрузить ноду"
-    echo "5. 🗑️ Удалить ноду"
-    echo -e "6. 🚪 Выйти из скрипта\n"
-    read -p "Выберите пункт меню: " choice
-
-    case $choice in
-      1)
-        download_node
-        ;;
-      2)
-        check_logs
-        ;;
-      3)
-        check_points
-        ;;
-      4)
-        restart_node
-        ;;
-      5)
-        delete_node
-        ;;
-      6)
-        exit_from_script
-        ;;
-      *)
-        echo "Неверный пункт. Пожалуйста, выберите правильную цифру в меню."
-        ;;
-    esac
-  done
